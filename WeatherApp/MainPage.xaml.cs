0bf0739d698a85f775ec9b1b21b708422490f200
vukosavljevic <domagojv2005@gmail.com>
2024-04-10 23:59:48 +0200

    initial commit

---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿namespace WeatherApp;
+
+public partial class MainPage : ContentPage
+{
+    private const string ApiKey = "bf3375071b3286d4c7997a194f6196d5"; // Replace with your OpenWeatherMap API key
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnGetWeatherClicked(object sender, EventArgs e)
+    {
+        string cityName = cityEntry.Text;
+
+        if (!string.IsNullOrEmpty(cityName))
+        {
+            WeatherData weatherData = await GetWeatherData(cityName);
+
+            if (weatherData != null)
+            {
+                temperatureLabel.Text = $"Temperature: {weatherData.Main.Temperature}°C";
+                pressureLabel.Text = $"Pressure: {weatherData.Main.Pressure} hPa";
+                humidityLabel.Text = $"Humidity: {weatherData.Humidity}%";
+                descriptionLabel.Text = $"Description: {weatherData.Weather[0].Description}";
+                windSpeedLabel.Text = $"Wind Speed: {weatherData.Wind.Speed} m/s";
+
+                // Update other labels with additional weather details as needed
+            }
+            else
+            {
+                // Handle error or inform user that data couldn't be retrieved
+            }
+        }
+        else
+        {
+            // Inform user to enter a city name
+        }
+    }
+
+    private async Task<WeatherData> GetWeatherData(string cityName)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={ApiKey}&units=metric";
+            string jsonResponse = await client.GetStringAsync(apiUrl);
+
+            WeatherData weatherData = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherData>(jsonResponse);
+
+            return weatherData;
+        }
+        catch (Exception ex)
+        {
+            // Handle exception, log, or return null
+            return null;
+        }
+    }
+}
+
---
+++ WeatherApp/WeatherApp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherApp
+{
+    public class WeatherData
+    {
+        [JsonProperty("main")]
+        public MainData Main { get; set; }
+
+        [JsonProperty("weather")]
+        public Weather[] Weather { get; set; }
+
+        [JsonProperty("name")]
+        public string CityName { get; set; }
+
+        [JsonProperty("wind")]
+        public WindData Wind { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+
+        // Add more properties as needed
+    }
+
+    public class MainData
+    {
+        [JsonProperty("temp")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("pressure")]
+        public double Pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        // You can add more properties like 'icon', 'main', etc. as needed
+    }
+
+    public class WindData
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+
+        // You can add more properties like 'deg', 'gust', etc. as needed
+    }
+
+}
